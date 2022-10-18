3f33d3bbf3421be3888870bba364339c4dd3ef5e
ahmedZread123 <yahmed9013@gmail.com>
2022-10-18 11:48:00 +0300

    update

--- src/BitlyServiceProvider.php
+++ src/BitlyServiceProvider.php
@@ -7,14 +7,14 @@ class BitlyServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'config/bitly.php' => config_path('bitly.php'),
+            __DIR__.'/config/bitly.php' => config_path('bitly.php'),
         ],'bitly');
     }
 
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__.'config/bitly.php', 'bitly'
+            __DIR__.'/config/bitly.php', 'bitly'
         );
     }
 }
